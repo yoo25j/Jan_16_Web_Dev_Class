f9f04fc78d7aadb36a21d713aa3fd08cb203d8c0
Demo User <demouser@n226-169.mtholyoke.edu>
2016-01-04 14:26:35 -0500

    Added URL to resolve the home page

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,6 +1,16 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from lists.views import home_page
+
+class HomePageTest(TestCase):
+#extends test case
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        #matches every string past domain name
+        #ex: app.me(**after this domain name)/lisis/123ab
+        self.assertEqual(found.func, home_page)
 
 
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1+1,3)
+# class SmokeTest(TestCase):
+#     def test_bad_maths(self): #failing test
+#         self.assertEqual(1+1,3)
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
+#home_page = None
+def home_page():
+    pass
 # Create your views here.
