a2946cef855f4bb5c8cb357e658b46bd06a9e8d4
Demo User <demouser@MacBook-Pro.local>
2016-01-05 11:21:49 -0500

    Expanded implementation of Edith's functional test

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -7,41 +8,39 @@ class NewVisitorTest(unittest.TestCase):
         self.browser = webdriver.Firefox() #new instance of firefox
         self.browser.implicitly_wait(3)
     def tearDown(self):
-        self.browser.quit()                # after every, shut down
+        self.browser.quit() # after every, shut down
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-
-    #def test_can_log_in_to_a_new_account(self):
-
-        # write test first before writing the program
         # Edith has heard abot a call new online to-do app
         # She goes to check out its homepage
-        #browser = webdriver.Firefox()
-        #start a portal 8000
-        #go to the homepage
         self.browser.get('http://localhost:8000')
         # She notices the page title and header mention to-do lists.
         self.assertIn('To-Do', self.browser.title)
-        #assert 'Django' in browser.title #only takes in boolean parameters
-
-        #the last line is similar to the following:
-        #if ! 'django' in browser.title:
-        #   throw new AssertionError
-
-
-        #functional test, acceptance test, End to End (meaning from the point view of user)
-
+        header_text = self.browser.find_element_by_tag_name('h1').text #find h1 tag on page, assuming it header we want
+        self.assertIn('To-Do', header_text)
 
         #She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_ new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter to-do item'
+        )
         # She types "Buy peacock feathers" into a textbox
         #her hobby is trying fly-fishing lures
-
+        inputbox.send_keys('Buy peacock featers')
         #When she hits enter, the page updates, and now the page lists
         #1. Buy peacock feathers as the item in a to-do lists
+        inputbox.send_keys(Keys.ENTER) # Keys is new thing we havne't referrred to yet, import it
 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_Tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1. Buy peacock featers' for row in rows) #useful in that its quick and dirty #right now it is becoming a bad decision, gets us further
+        )
         #There is still a textbox inviting her to add another item
         # She enters 'Use peacock feathers to make fly'
         # She is very methdolical
+
         # The homepage updates again, and now shows both items on her lists
 
         #Edith wonders whether the site will remember her list. Then she sees
