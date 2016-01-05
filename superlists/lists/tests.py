cd2bbe7c4deb2a7036af1599e20e5a8bc93f27c6
Demo User <demouser@MacBook-Pro.local>
2016-01-05 11:22:33 -0500

    Added home page template

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,26 +1,26 @@
 from django.core.urlresolvers import resolve
+from django.template.loader import render_to_string
 from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
 
 
-class HomePageTest(TestCase):
-#extends test case
+class HomePageTest(TestCase): #extends test case
     def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        #matches every string past domain name
-        #ex: app.me(**after this domain name)/lisis/123ab
+        found = resolve('/') #matches every string past domain name #ex: app.me(**after this domain name)/lisis/123ab
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
+        request = HttpRequest() #represents url request, python abstraction for what a browser what look for that value
         response = home_page(request)
+        expected_html = render_to_string('home.html') #after deleting tests below, put in this
+        self.assertEqual(response.content.decode(), expected_html)
 
-        self.assertTrue(response.content.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith('</html>'))
+        # self.assertTrue(response.content.startswith('<html>'))
+        # self.assertIn('<title>To-Do lists</title>', response.content)
+        # self.assertTrue(response.content.strip().endswith('</html>'))
 
 
-# class SmokeTest(TestCase):
-#     def test_bad_maths(self): #failing test
-#         self.assertEqual(1+1,3)
+        # class SmokeTest(TestCase):
+        #     def test_bad_maths(self): #failing test
+        #         self.assertEqual(1+1,3)
--- superlists/superlists/settings.py
+++ superlists/superlists/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'lists', #added to new additions to app
 ]
 
 MIDDLEWARE_CLASSES = [
