5a67d08fc86db761df7d55d3e9915c7acefb66c8
Demo User <demouser@MacBook-Pro.local>
2016-01-06 13:33:13 -0500

    Migrated functional tests into own app

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,8 +1,8 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox() #new instance of firefox
@@ -23,7 +23,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
 
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         # She notices the page title and header mention to-do lists.
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text #find h1 tag on page, assuming it header we want
@@ -47,28 +47,3 @@ class NewVisitorTest(unittest.TestCase):
         self.check_for_row_in_list_table('2. Use peacock feathers to make a fly')
 
         self.fail('Finish the app!') #until we remove this line, it'll be a failure
-
-if __name__ == '__main__':
-    unittest.main() #main method, ignore warning
-
-# Edith has heard abot a call new online to-do app
-# She goes to check out its homepage
-
-# inputbox.send_keys('Buy peacock feathers')
-#When she hits enter, the page updates, and now the page lists
-#1. Buy peacock feathers as the item in a to-do lists
-
-#There is still a textbox inviting her to add another item
-# She enters 'Use peacock feathers to make fly'
-# She is very methdolical
-
-# The homepage updates again, and now shows both items on her lists
-
-#Edith wonders whether the site will remember her list. Then she sees
-#that the site has generated a unique URL for her -- there is some
-#explanatory text to that effect.
-
-#She visits that URL - her to-do list is still there
-
-#satisfied, she goes back to sleep
-#browser.quit()
