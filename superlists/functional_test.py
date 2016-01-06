b2b1375e3a131274ca3ff3447109a605654b258f
Demo User <demouser@MacBook-Pro.local>
2016-01-06 10:45:22 -0500

    Refactored functional tests

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -7,8 +7,19 @@ class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox() #new instance of firefox
         self.browser.implicitly_wait(3)
+
     def tearDown(self):
         self.browser.quit() # after every, shut down
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
+    def enter_a_new_item(self, todo_text):
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys(todo_text)
+        inputbox.send_keys(Keys.ENTER)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard abot a call new online to-do app
@@ -19,24 +30,25 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text #find h1 tag on page, assuming it header we want
         self.assertIn('To-Do', header_text)
 
+        inputbox = self.browser.find_element_by_id('id_new_item')
         #She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_id('id_ new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
-            'Enter to-do item'
+            'Enter a To-Do item'
         )
         # She types "Buy peacock feathers" into a textbox
         #her hobby is trying fly-fishing lures
-        inputbox.send_keys('Buy peacock featers')
+        self.enter_a_new_item('Buy peacock feathers')
+        # inputbox.send_keys('Buy peacock feathers')
         #When she hits enter, the page updates, and now the page lists
         #1. Buy peacock feathers as the item in a to-do lists
         inputbox.send_keys(Keys.ENTER) # Keys is new thing we havne't referrred to yet, import it
+        self.check_for_row_in_list_table('1. Buy peacock feathers')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_Tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1. Buy peacock featers' for row in rows) #useful in that its quick and dirty #right now it is becoming a bad decision, gets us further
-        )
+        self.enter_a_new_item('Use peacock feathers to make a fly')
+
+        self.check_for_row_in_list_table('1. Buy peacock feathers')
+        self.check_for_row_in_list_table('2. Use peacock feathers to make a fly')
         #There is still a textbox inviting her to add another item
         # She enters 'Use peacock feathers to make fly'
         # She is very methdolical
