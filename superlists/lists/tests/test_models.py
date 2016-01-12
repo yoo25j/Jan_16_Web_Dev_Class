c199decf791a0424cf8a5adbb9cbfe0bb8372a32
Demo User <demouser@MacBook-Pro-3.local>
2016-01-12 14:17:32 -0500

    Split out unit tests into two files

---
+++ superlists/lists/tests/test_models.py
@@ -0,0 +1,41 @@
+from django.test import TestCase
+from lists.models import Item, List
+
+class ItemAndListModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        new_list = List()
+        new_list.save()
+
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.list = new_list
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.list = new_list
+        second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, new_list)
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, new_list)
+        self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, new_list)
+
+
+        # self.assertTrue(response.content.startswith('<html>'))
+        # self.assertIn('<title>To-Do lists</title>', response.content)
+        # self.assertTrue(response.content.strip().endswith('</html>'))
+
+
+        # class SmokeTest(TestCase):
+        #     def test_bad_maths(self): #failing test
+        #         self.assertEqual(1+1,3)
