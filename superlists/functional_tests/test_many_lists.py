20cce483bd1cdddf6adc7aa16f4188fc70a8abf7
Demo User <demouser@MacBook-Pro-3.local>
2016-01-14 13:48:30 -0500

    FT's: added multi list fx

---
+++ superlists/functional_tests/test_many_lists.py
@@ -0,0 +1,30 @@
+from .base import TodoFunctionalTest
+
+class ManyListsTest(TodoFunctionalTest):
+
+    def change_list_name(self,list_name):
+        pass
+
+    def test_can_create_and_view_multiple_lists(self):
+      self.browser.get(self.live_server_url)
+      self.enter_a_new_item('Buy milk')
+      self.enter_a_new_item('Buy cheese')
+      self.check_for_row_in_list_table('Buy milk')
+      self.check_for_row_in_list_table('Buy cheese')
+
+      #she sees she can change a lit name
+      self.change_list_name('Groceries')
+
+      self.browser.get(self.live_server_url)
+      self.check_for_row_in_list_table('Groceries')
+
+      #edith creates a new list for her art history hw
+      self.browser.enter_a_new_item('Read Camille')
+      self.browser.get(self.live_server_url)
+      self.check_for_row_in_list_table('Groceries')
+      self.check_for_row_in_list_table('Read Camille')
+
+      row = self.find_table_row('Groceries')
+      row.find_element_by_tag_name('a').click()
+      self.check_for_row_in_list_table('Buy milk')
+      self.check_for_row_in_list_table('Buy cheese')
